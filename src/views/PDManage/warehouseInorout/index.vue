988d4c1f494dc4f750018e0122de395feb2fc94b
Galaxy_G <269274122@qq.com>
2019-02-26 16:49:17 +0800

    发货表单制作

--- src/views/PDManage/warehouseInorout/index.vue
+++ src/views/PDManage/warehouseInorout/index.vue
@@ -36,31 +36,31 @@
     </el-table-column>
     <el-table-column align="center" label="单位">
       <template slot-scope="scope">
-        <span >
+        <span>
           {{ scope.row.company}}</span>
       </template>
     </el-table-column>
     <el-table-column align="center" label="仓库">
       <template slot-scope="scope">
-        <span >
+        <span>
           {{ scope.row.warehouse}}</span>
       </template>
     </el-table-column>
     <el-table-column align="center" label="负责人">
       <template slot-scope="scope">
-        <span >
+        <span>
           {{ scope.row.userName}}</span>
       </template>
     </el-table-column>
     <el-table-column align="center" label="单位">
       <template slot-scope="scope">
-        <span >
+        <span>
           {{ scope.row.company}}</span>
       </template>
     </el-table-column>
     <el-table-column align="center" label="备注">
       <template slot-scope="scope">
-        <span >
+        <span>
           {{ scope.row.remark}}</span>
       </template>
     </el-table-column>
@@ -87,36 +87,157 @@
   </div>
 
 
-  <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible" width="30%" top='5%'>
+  <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible" width="80%" top='2%'>
 
-    <el-form class="" label-width="20%" style="text-align:left">
+    <el-form class="" label-width="20%" style="text-align:left;padding:0 10% ">
+      <div class="" style="border:1px solid#ccc;">
+        <h3 style="margin-left:30px;">单据信息</h3>
+        <el-row :gutter="24">
+          <el-col :span="12">
+            <el-form-item label="单据编号">
+              <el-input v-model="obj.receipt" placeholder="请输入单据编号" style="width:80%"></el-input>
+            </el-form-item>
 
-      <el-row :gutter="24">
+            <el-form-item label="录单时间">
+              <el-input v-model="obj.timestamp" placeholder="请输入录单时间" style="width:80%"></el-input>
+            </el-form-item>
 
-        <el-form-item label="仓库">
-          <el-input v-model="obj.warehouse" placeholder="请输入仓库" style="width:80%"></el-input>
-        </el-form-item>
-        <el-form-item label="单位">
-          <el-input v-model="obj.company" placeholder="请输入单位" style="width:80%"></el-input>
-        </el-form-item>
+            <el-form-item label="出入仓库">
+              <el-input v-model="obj.warehouse" placeholder="请输入出入仓库" style="width:80%"></el-input>
+            </el-form-item>
+            <el-form-item label="供应商/经销商">
+              <el-input v-model="obj.company" placeholder="请输入供应商/经销商" style="width:80%"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="货物管理类型">
+              <el-select v-model="obj.type" style="width:150px" placeholder="请选择">
+                <el-option label="出库" :key=1 :value=1>
+                </el-option>
+                <el-option label="入库" :key=2 :value=2>
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="负责人">
+              <el-input v-model="obj.userName" placeholder="请输入负责人" style="width:80%"></el-input>
+            </el-form-item>
+            <el-form-item label="备注">
+              <el-input v-model="obj.remark" placeholder="请输入备注" style="width:80%"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
 
-        <el-form-item label="货物管理类型">
-          <el-select v-model="obj.type" style="width:150px" placeholder="请选择">
-            <el-option label="出库" :key=1 :value=1>
-            </el-option>
-            <el-option label="入库" :key=2 :value=2>
-            </el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="负责人">
-          <el-input v-model="obj.userName" placeholder="请输入负责人" style="width:80%"></el-input>
-        </el-form-item>
-        <el-form-item label="备注">
-          <el-input v-model="obj.remark" placeholder="请输入备注" style="width:80%"></el-input>
-        </el-form-item>
+      </div>
+      <div class="" style="border:1px solid#ccc;margin-top:30px;">
+        <h3 style="margin-left:30px;">添加货品</h3>
+        <el-table v-loading="loading" class="tableH" :data="goodsList" border style="margin-top:20px;width:100%;font-size:12px;">
+          <el-table-column type="index" width="50" align="center" label="ID">
+          </el-table-column>
+          <el-table-column align="center" label="货品图片">
+            <template slot-scope="scope">
+              <img :src=scope.row.thumbUrl alt="">
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="货品名称">
+            <template slot-scope="scope">
+              <span>{{ scope.row.name }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="分类">
+            <template slot-scope="scope">
+              <span>
+                {{ scope.row.goodsType}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="库存数量">
+            <template slot-scope="scope">
+              <span>
+                {{ scope.row.size}}</span>
+            </template>
+          </el-table-column>
 
-      </el-row>
+          <el-table-column align="center" label="入库参考价">
+            <template slot-scope="scope">
+              <span>
+                {{ scope.row.inprice}}</span>
+            </template>
+          </el-table-column>
 
+          <el-table-column align="center" label="出库参考价">
+            <template slot-scope="scope">
+              <span>
+                {{ scope.row.outprice}}</span>
+            </template>
+          </el-table-column>
+
+          <el-table-column align="center" label="">
+            <template slot-scope="scope">
+              <span>
+                <el-input type="number" v-model="scope.row.num" placeholder="数量"  style="width:80%"></el-input>
+              </span>
+            </template>
+          </el-table-column>
+
+          <el-table-column align="center" label="">
+            <template slot-scope="scope">
+              <span>
+                <el-input type="number" v-model="scope.row.price" placeholder="单价"  style="width:80%"></el-input>
+              </span>
+            </template>
+          </el-table-column>
+
+          <el-table-column align="center" label="添加">
+            <template slot-scope="scope">
+              <el-button size="small" @click="handleAdd(scope.row)" type="" class="el-icon-circle-plus" style="border:0px;font-size:20px;"></el-button>
+            </template>
+          </el-table-column>
+
+        </el-table>
+      </div>
+
+      <div class="" style="border:1px solid#ccc;margin-top:30px;">
+        <h3 style="margin-left:30px;">货品列表</h3>
+        <el-table v-loading="loading" class="tableH" :data="nowGoodsList" border style="margin-top:20px;width:100%;font-size:12px;">
+          <el-table-column type="index" width="50" align="center" label="ID">
+          </el-table-column>
+          <el-table-column align="center" label="货品图片">
+            <template slot-scope="scope">
+              <img :src=scope.row.thumbUrl alt="">
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="货品名称">
+            <template slot-scope="scope">
+              <span>{{ scope.row.name }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="分类">
+            <template slot-scope="scope">
+              <span>
+                {{ scope.row.goodsType}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="数量">
+            <template slot-scope="scope">
+              <span>
+                {{ scope.row.num}}</span>
+            </template>
+          </el-table-column>
+
+          <el-table-column align="center" label="单价">
+            <template slot-scope="scope">
+              <span>
+                {{ scope.row.price}}</span>
+            </template>
+          </el-table-column>
+
+          <el-table-column align="center" label="操作">
+            <template slot-scope="scope">
+              <el-button size="small" @click="handleGoodsEdit(scope.row)" type="" class="el-icon-delete colorred borderred"></el-button>
+            </template>
+          </el-table-column>
+
+        </el-table>
+      </div>
 
     </el-form>
 
@@ -142,6 +263,9 @@ import {
 } from '@/api/warehouseInorout'
 
 
+import {
+  getGoodsList,
+} from '@/api/goods'
 
 export default {
   mixins: [mixin], // 使用mixins
@@ -164,6 +288,8 @@ export default {
         operator: '',
         operatorTime: '',
       },
+      nowGoodsList: [],
+      goodsList: []
     }
   },
   created() {
@@ -176,6 +302,63 @@ export default {
   },
   computed: {},
   methods: {
+    handleAdd(obj) {
+      if (!obj.num || !obj.price) {
+        this.$message({
+          message: '数量和单价不能为空！',
+          type: 'error',
+          duration: 5 * 1000
+        })
+        return
+      }
+      obj.size = parseInt(obj.size) - parseInt(obj.num)
+      if (obj.size < 0) {
+        this.$message({
+          message: '库存不足！',
+          type: 'error',
+          duration: 5 * 1000
+        })
+        obj.size = parseInt(obj.size) + parseInt(obj.num)
+        return
+      }
+      let data = this.deepCopy(obj)
+      let list = this.nowGoodsList
+      let flag = true
+      if (list.length > 0) {
+        for (let x in list) {
+          if (list[x].id == data.id) {
+            list[x].num = parseInt(list[x].num) + parseInt(data.num)
+            flag = false
+            continue
+          }
+        }
+        if (flag) {
+          this.nowGoodsList.push(data)
+        }
+
+      } else {
+        this.nowGoodsList.push(data)
+      }
+    },
+    handleGoodsEdit(obj) {
+      let list = this.goodsList
+      let flag = true
+      for (let x in list) {
+        if (list[x].id == obj.id) {
+          list[x].size = parseInt(list[x].size) + parseInt(obj.num)
+          flag = false
+          continue
+        }
+      }
+      if (!flag) {
+        this.nowGoodsList.splice(this.nowGoodsList.indexOf(obj), 1)
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        });
+      }
+
+    },
     async loadPageList() {
       if (this.input) {
         this.listQuery.objName = this.input.objName
@@ -194,7 +377,27 @@ export default {
         this.loading = false
       }
     },
-
+    async loadGoodsList() {
+      if (this.input) {
+        this.listQuery.objName = this.input.objName
+      } else {
+        this.listQuery.objName = ''
+      }
+      let {
+        data,
+        success,
+        message
+      } = await getGoodsList(this.listQuery)
+      if (success) {
+        let goodsList = data.list
+        for (let x in goodsList) {
+          goodsList[x].thumbUrl = this.imgBaseUrl + goodsList[x].thumbUrl
+        }
+        this.goodsList = goodsList
+        // this.total = data.total
+        this.loading = false
+      }
+    },
     async handleCreate() {
       this.dialogStatus = 'create'
       this.dialogFormVisible = true
@@ -209,6 +412,7 @@ export default {
         operator: '',
         operatorTime: '',
       }
+      this.loadGoodsList()
     },
 
 
@@ -251,6 +455,7 @@ export default {
         this.dialogsave = true
         this.dialogadd = false
         this.dialogFormVisible = true
+        this.loadGoodsList()
       } else if (type === 'del') {
         this.$confirm('此操作将删除该记录, 是否继续?', '提示', {
           confirmButtonText: '确定',
